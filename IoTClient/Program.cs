1faa4f0c4c60a345275723ca29b6f5b809ea8a85
gentz <gentz@SURFACE-GENTZOS>
2017-04-22 11:58:30 +0200

    Add project files.

---
+++ IoTClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+
+namespace IoTClient
+{
+    class Program
+    {
+        static ServiceClient serviceClient;
+        static string connectionString = "";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Send Cloud-to-Device message\n");
+            serviceClient = ServiceClient.CreateFromConnectionString(connectionString);
+
+            Console.WriteLine("Press any key to send a C2D message.");
+            Console.ReadLine();
+            SendCloudToDeviceMessageAsync().Wait();
+            Console.ReadLine();
+        }
+
+        private async static Task SendCloudToDeviceMessageAsync()
+        {
+            var commandMessage = new Message(Encoding.ASCII.GetBytes("5"));
+            await serviceClient.SendAsync("thermostat01", commandMessage);
+        }
+    }
+}
